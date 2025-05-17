08094084dfbb7bd6b4e1cf58653bae39098db13d
Denis <Make772@ukr.net>
2025-05-17 13:09:26 +0300

    rename app -> books & user -> users & add borrowings app

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'app',
-    'user',
+    'books',
+    'users',
 ]
 
 MIDDLEWARE = [
