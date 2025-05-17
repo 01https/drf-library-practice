f47d5fca3480303a38a75c4f335449c89c9dc1c9
Denis <Make772@ukr.net>
2025-05-17 21:32:35 +0300

    add router for BookViewSet

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
-    path("api/library/", include("books.urls", namespace="books"))
+    path("api/", include("books.urls", namespace="books"))
 ]
