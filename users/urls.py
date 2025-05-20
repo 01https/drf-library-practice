eecd45eef28e67e456343a86980788638b6cf9af
Denis <Make772@ukr.net>
2025-05-20 18:32:36 +0300

    add path to JWT token/refresh and me & register

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,17 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from users.views import ManageUserView, CreateUserView
 
 
-urlpatterns = []
+urlpatterns = [
+    path("user/", CreateUserView.as_view(), name="register"),
+    path("user/token/", TokenObtainPairView.as_view(), name="token"),
+    path("user/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("user/me/", ManageUserView.as_view(), name="me"),
+]
+
+app_name = "users"
