2d61628dea85be4b59a3a5593f038ff68add1a24
Denis <Make772@ukr.net>
2025-05-20 20:03:22 +0300

    add authentication class & permission class in BookViewSet

--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from books.permissions import IsAdminOrReadOnly
 from books.serializers import BookSerializer
 from books.models import Book
 
@@ -7,3 +9,5 @@ from books.models import Book
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminOrReadOnly]
